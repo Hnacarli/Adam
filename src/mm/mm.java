a983ef81bafa8b0454e8b31c5a0f59752bd45c99
Hnacarli <hnacarli@nelta.de>
2023-02-07 15:09:50 +0100

    Neu eingerichtet

---
+++ src/mm/mm.java
@@ -0,0 +1,25 @@
+package mm;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.concurrent.TimeUnit;
+
+public class mm {
+    public static WebDriver driver;
+    public void launcher() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Hanifi Nacarli\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        ChromeOptions cr = new ChromeOptions();
+        cr.addArguments("--incognito");
+        driver = new ChromeDriver(cr);
+        driver.manage().window().maximize();
+    }
+
+    public static void main(String[] args){
+        mm script = new mm();
+        script.launcher();
+    }
+}
+
